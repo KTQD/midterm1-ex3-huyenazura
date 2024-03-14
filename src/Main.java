8289c9335f6a208c6463e39a206a487e4542cd58
huyenazura <145855494+huyenazura@users.noreply.github.com>
2024-03-14 08:50:09 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.io.*;
+
+
 public class Main {
     public static void main(String[] args) {
+        String inputFileName = "input.txt";
+        String outputFileName = "output.txt";
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) 
+             {
 
-        Bài làm viết tại đây
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Thay thế từ "Nha Trang" bằng "Vũng Tàu"
+                line = line.replace("Nha Trang", "Vũng Tàu");
+                writer.write(line);
+                writer.newLine();
+            }
 
+            System.out.println("Đã thay thế và ghi nội dung vào file output.txt");
+
+        } catch (IOException e) {
+            System.err.println("Đã xảy ra lỗi khi đọc hoặc ghi file: " + e.getMessage());
+        }
     }
 }
